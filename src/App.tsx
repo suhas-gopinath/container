354113fc2d7a3f7d668a46c124191e29723a9b9e
suhas-gopinath <emailofsuhas@gmaul.com>
2026-03-28 13:05:56 +0530

    final

--- src/App.tsx
+++ src/App.tsx
@@ -7,51 +7,50 @@ import { Verify } from "./components/Verify";
 import "./index.css";
 import "./App.css";
 import Fallback from "./components/Fallback";
-import { Login } from "login/Login";
-import { Register } from "register/Register";
+// import { Login } from "login/Login";
+// import { Register } from "register/Register";
 
-// const Login = React.lazy(() =>
-//   import("login/Login").then((module) => ({
-//     default: module.Login,
-//   })),
-// );
+const Login = React.lazy(() =>
+  import("login/Login").then((module) => ({
+    default: module.Login,
+  })),
+);
 
-// const Register = React.lazy(() =>
-//   import("register/Register").then((module) => ({
-//     default: module.Register,
-//   })),
-// );
+const Register = React.lazy(() =>
+  import("register/Register").then((module) => ({
+    default: module.Register,
+  })),
+);
 
 const App = () => (
   <div className="app-wrapper">
     <div className="container">
       <header className="app-header">
         <h1 className="app-title">Host Container App</h1>
-        <p className="app-subtitle">Microfrontend Architecture</p>
       </header>
       <BrowserRouter>
-        {/* <React.Suspense
+        <React.Suspense
           fallback={
             <div className="loading-container">
               <div className="loading-spinner" role="status"></div>
             </div>
           }
-        > */}
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/verify"
-            element={
-              <MessageProvider>
-                <MessageDisplay />
-                <Verify />
-              </MessageProvider>
-            }
-          />
-          <Route path="*" element={<Fallback />} />
-        </Routes>
-        {/* </React.Suspense> */}
+        >
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/verify"
+              element={
+                <MessageProvider>
+                  <MessageDisplay />
+                  <Verify />
+                </MessageProvider>
+              }
+            />
+            <Route path="*" element={<Fallback />} />
+          </Routes>
+        </React.Suspense>
       </BrowserRouter>
     </div>
   </div>
